b3c31f229c1c1411bf14a0c1ba565d53129983bc
grace-mao23 <gmao00@stuy.edu>
2018-11-01 14:47:15 -0400

    Other add method overrided, none tested

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -22,4 +22,11 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     return super.add(value);
   }
 
+  public void add(int index, T value) {
+    if (value == null) {
+      throw new IllegalArgumentException("Cannot set value to null");
+    }
+    super.add(index, value);
+  }
+
 }
