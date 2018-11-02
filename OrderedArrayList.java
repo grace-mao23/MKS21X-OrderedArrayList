22dbb39a1d52eb0736eae0d1a9c4d352b837806b
grace-mao23 <gmao00@stuy.edu>
2018-11-02 14:36:49 -0400

    Fixed set after testing

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -10,12 +10,13 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   public boolean add(T value) {
     for (int i = 0; i < size(); i++) {
-      if (value.compareTo(this.get(i)) > 0) {
-        super.add(i + 1, value);
+      if (value.compareTo(this.get(i)) <= 0) {
+        super.add(i, value);
         return true;
       }
     }
-    return false;
+    super.add(0, value);
+    return true;
   }
 
   public void add(int index, T value) {
