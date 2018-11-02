9e51bc0113b2e314d01c4899a942f5fe4890f837
grace-mao23 <gmao00@stuy.edu>
2018-11-01 20:29:23 -0400

    Overrided first add method, NOT tested

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,4 +7,14 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int starting) {
     super(starting);
   }
+
+  public boolean add(T value) {
+    for (int i = 0; i < size(); i++) {
+      if (value.compareTo(this.get(i)) > 0) {
+        super.add(i + 1, value);
+        return true;
+      }
+    }
+    return false;
+  }
 }
